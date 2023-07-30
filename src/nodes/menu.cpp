ea393d810a268240c64e26c8e50abdd9fa346ce2
kraxarn <kraxie@protonmail.com>
2023-07-30 13:54:22 +0200

    Show some debug info on menu

--- src/nodes/menu.cpp
+++ src/nodes/menu.cpp
@@ -1,6 +1,8 @@
 #include "nodes/menu.hpp"
 
 #include <godot_cpp/classes/scene_tree.hpp>
+#include <godot_cpp/classes/os.hpp>
+#include <godot_cpp/classes/rendering_server.hpp>
 
 Menu::Menu()
 	: start_game(nullptr),
@@ -35,6 +37,9 @@ void Menu::_ready()
 	quit_game->connect("pressed", {this, "_on_QuitGame_pressed"});
 
 	settings_window = get_node<godot::Window>("SettingsWindow");
+
+	auto *debug_info = get_node<godot::Label>("DebugInfo");
+	debug_info->set_text(get_system_info());
 }
 
 void Menu::_on_StartGame_pressed()
@@ -49,4 +54,33 @@ void Menu::_on_Settings_pressed()
 void Menu::_on_QuitGame_pressed()
 {
 	get_tree()->quit();
+}
+
+auto Menu::get_system_info() -> godot::String
+{
+	const auto *os_info = godot::OS::get_singleton();
+	auto *render_info = godot::RenderingServer::get_singleton();
+
+	godot::Dictionary values;
+	values["name"] = os_info->get_name();
+	values["distribution_name"] = os_info->get_distribution_name();
+	values["locale_language"] = os_info->get_locale_language();
+	values["processor_count"] = os_info->get_processor_count();
+	values["processor_name"] = os_info->get_processor_name();
+
+	values["video_adapter_name"] = render_info->get_video_adapter_name();
+	values["video_adapter_vendor"] = render_info->get_video_adapter_vendor();
+	values["video_adapter_api_version"] = render_info->get_video_adapter_api_version();
+
+	if (os_info->is_debug_build())
+	{
+		values["title"] = "Debug Mode";
+	}
+
+	return godot::String(
+		"{title}\n"
+		"OS: {name} ({distribution_name})\n"
+		"CPU: {processor_name} ({processor_count} cores)\n"
+		"GPU: {video_adapter_vendor} - {video_adapter_name} (OpenGL {video_adapter_api_version})\n"
+	).format(values);
 }
--- src/nodes/menu.hpp
+++ src/nodes/menu.hpp
@@ -3,6 +3,7 @@
 #include <godot_cpp/classes/node.hpp>
 #include <godot_cpp/classes/button.hpp>
 #include <godot_cpp/classes/window.hpp>
+#include <godot_cpp/classes/label.hpp>
 
 class Menu: public godot::Node
 {
@@ -25,4 +26,6 @@ private:
 	godot::Button *settings;
 	godot::Button *quit_game;
 	godot::Window *settings_window;
+
+	static auto get_system_info() -> godot::String;
 };
