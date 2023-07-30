a0c828125fce4e7d946d6ea4a003531d9256d648
kraxarn <kraxie@protonmail.com>
2023-07-30 19:19:15 +0200

    Add format_size

--- src/util.cpp
+++ src/util.cpp
@@ -7,3 +7,35 @@ auto util::to_vector2(const godot::Vector2i &vec2) -> godot::Vector2
 		static_cast<real_t>(vec2.y),
 	};
 }
+
+auto util::format_size(int64_t bytes) -> godot::String
+{
+	constexpr int64_t kilo = 1'000;
+	constexpr int64_t mega = 1'000'000;
+	constexpr int64_t giga = 1'000'000'000;
+
+	godot::Dictionary values;
+
+	if (bytes >= giga)
+	{
+		values["size"] = bytes / giga;
+		values["unit"] = "GB";
+	}
+	else if (bytes >= mega)
+	{
+		values["size"] = bytes / mega;
+		values["unit"] = "MB";
+	}
+	else if (bytes >= kilo)
+	{
+		values["size"] = bytes / kilo;
+		values["unit"] = "KB";
+	}
+	else
+	{
+		values["size"] = bytes;
+		values["unit"] = "B";
+	}
+
+	return godot::String("{size} {unit}").format(values);
+}
--- src/util.hpp
+++ src/util.hpp
@@ -9,6 +9,8 @@ class util
 public:
 	static auto to_vector2(const godot::Vector2i &vec2) -> godot::Vector2;
 
+	static auto format_size(int64_t bytes) -> godot::String;
+
 private:
 	util() = default;
 };
