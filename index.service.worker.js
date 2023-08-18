149d1d9a95452baba467a531df05249a80261985
kraxarn <6675728+kraxarn@users.noreply.github.com>
2023-08-18 19:15:26 +0000

    Deploying to gh-pages from @ kraxarn/xonix@f366e0b51890c39c73ea865b16db6b7276a14d10 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1692382290|5348602";
+const CACHE_VERSION = "1692386123|4253474";
 const CACHE_PREFIX = "xonix-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
